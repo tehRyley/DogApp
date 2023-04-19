0dc9963617328ad969af69e38a8d2298ccd6424b
mikesouls <mm0965773@otc.edu>
2023-04-19 08:55:02 -0500

    Testing API Stuff

--- app/src/main/java/com/example/apiapp/MainActivity.java
+++ app/src/main/java/com/example/apiapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.apiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     String key = "yNyg0AMeLuPdBjE7Q84wVg==0wTb8nJHdRkOgD2p";
@@ -11,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        URL url = new URL("https://api.api-ninjas.com/v1/quotes?category=happiness");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("accept", "application/json");
+        InputStream responseStream = connection.getInputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(responseStream);
+        System.out.println(root.path("fact").asText());
     }
 }
