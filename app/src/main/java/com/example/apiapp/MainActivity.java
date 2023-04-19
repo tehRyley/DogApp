023b865d0a4c0c79c3816f31841712b9531b1cec
tehRyley <rg0901375@otc.edu>
2023-04-19 14:24:33 -0500

    Adjusted Layout Added dog breed text Moved request to own method for setting dog image and breed Added call for request method on create Added button to call request method Cleaned Warnings

--- app/src/main/java/com/example/apiapp/MainActivity.java
+++ app/src/main/java/com/example/apiapp/MainActivity.java
@@ -1,21 +1,18 @@
 package com.example.apiapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
-    String key = "yNyg0AMeLuPdBjE7Q84wVg==0wTb8nJHdRkOgD2p";
     String linkURL = "https://dog.ceo/api/breeds/image/random";
     TextView textView;
     ImageView imageView;
@@ -24,24 +21,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Button to call request method
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(view -> imgRequest());
+
         // help from https://www.tutorialspoint.com/how-to-use-volley-string-request-in-android
         textView = findViewById(R.id.textBox);
         imageView = findViewById(R.id.imageView);
         queue = Volley.newRequestQueue(this);
-        StringRequest request = new StringRequest(Request.Method.GET, linkURL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                String trimRes = response.toString().substring(response.indexOf("\"h")+1, response.lastIndexOf("\","));
-                Picasso.get().load(trimRes).into(imageView);
-                textView.setText(response.toString());
-                //Toast.makeText(MainActivity.this,trimRes,Toast.LENGTH_LONG).show();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d("error",error.toString());
-            }
-        });
+        imgRequest();
+    }
+
+    // Method for requesting and setting dog images.
+    public void imgRequest() {
+        StringRequest request = new StringRequest(Request.Method.GET, linkURL, response -> {
+            String url = response.substring(response.indexOf("\"h")+1, response.lastIndexOf("\","));
+            String breed = response.substring(response.indexOf("s\\/")+3, response.lastIndexOf("\\/"));
+            Picasso.get().load(url).into(imageView);
+            textView.setText(breed);
+        }, error -> Log.d("error",error.toString()));
         queue.add(request);
     }
 }
