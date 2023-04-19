f23c91fe3c3c6f82aa999e7ec06276606c3932f0
tehRyley <rg0901375@otc.edu>
2023-04-19 13:31:29 -0500

    Trimmed response for URL Implemented picasso Added simple image loading

--- app/src/main/java/com/example/apiapp/MainActivity.java
+++ app/src/main/java/com/example/apiapp/MainActivity.java
@@ -1,32 +1,24 @@
 package com.example.apiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
-import org.w3c.dom.Text;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
     String key = "yNyg0AMeLuPdBjE7Q84wVg==0wTb8nJHdRkOgD2p";
     String linkURL = "https://dog.ceo/api/breeds/image/random";
     TextView textView;
+    ImageView imageView;
     RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +26,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // help from https://www.tutorialspoint.com/how-to-use-volley-string-request-in-android
         textView = findViewById(R.id.textBox);
+        imageView = findViewById(R.id.imageView);
         queue = Volley.newRequestQueue(this);
         StringRequest request = new StringRequest(Request.Method.GET, linkURL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                String trimRes = response.toString().substring(response.indexOf("\"h")+1, response.lastIndexOf("\","));
+                Picasso.get().load(trimRes).into(imageView);
                 textView.setText(response.toString());
-                Toast.makeText(MainActivity.this,response.toString(),Toast.LENGTH_LONG).show();
+                //Toast.makeText(MainActivity.this,trimRes,Toast.LENGTH_LONG).show();
             }
         }, new Response.ErrorListener() {
             @Override
